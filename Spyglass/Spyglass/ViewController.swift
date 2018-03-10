09c91b65a62a144cd53515d73f0fa9e0b319eb2b
Eren Livingstone <eren.livingstone@gmail.com>
2018-03-10 17:46:03 -0500

    Initializing CoreML model, request, and handling classification results

--- Spyglass/Spyglass/ViewController.swift
+++ Spyglass/Spyglass/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate
+{
     @IBOutlet var sceneView: ARSCNView!
+	
+	var visionRequests = [VNRequest]()
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+	{
         super.viewDidLoad()
         
         // Set the view's delegate
@@ -28,9 +32,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+		
+		guard let visionModel = try? VNCoreMLModel(for: Inceptionv3().model) else
+		{
+			fatalError("Could not load InceptionV3 CoreML model")
+		}
+		
+		let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+		classificationRequest.imageCropAndScaleOption = .centerCrop
+		visionRequests = [classificationRequest]
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool)
+	{
         super.viewWillAppear(animated)
         
         // Create a session configuration
@@ -40,14 +54,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
+    override func viewWillDisappear(_ animated: Bool)
+	{
         super.viewWillDisappear(animated)
         
         // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+	{
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
@@ -63,18 +79,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 */
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
+    func session(_ session: ARSession, didFailWithError error: Error)
+	{
         // Present an error message to the user
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
+    func sessionWasInterrupted(_ session: ARSession)
+	{
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
         
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
+    func sessionInterruptionEnded(_ session: ARSession)
+	{
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+	
+	// MARK: - VisionCoreMLRequest handler
+	
+	func handleClassifications(request: VNRequest, error: Error?)
+	{
+		guard error == nil else
+		{
+			print("Classification error: \(error!.localizedDescription)")
+			return
+		}
+		
+		guard let observations = request.results else
+		{
+			print("Classification: no results")
+			return
+		}
+		
+		let classifications = observations[0...4] // top 4 results
+			.flatMap({ $0 as? VNClassificationObservation })
+			.map({ "\($0.identifier) \(($0.confidence * 100.0).rounded())" })
+			.joined(separator: "\n")
+		
+		DispatchQueue.main.async
+		{
+			// TODO: self.resultView.text = classifications
+		}
+	}
 }
